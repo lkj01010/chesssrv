d780c6869b1d5fcaca0abfbf68f81a3f530af1a1
mid <86190827@qq.com>
2016-02-22 18:00:58 +0800

    fix jsonencoder update login proto

--- login/proto.go
+++ login/proto.go
@@ -1,7 +1,30 @@
 package login
+import (
+)
+
+const (
+	methodRegister = 1000
+	methodLogin = 1001
+)
+
+const methodText = &map[int]string{
+	methodRegister:    "register",
+	methodLogin:    "login",
+}
+
+//var loginMethodCodes map[string]int
+//
+//func init() {
+//	if loginMethodCodes, err := json.Marshal(loginMethodCodeJson); err != nil {
+//		fw.Log.WithField("json.Marshal.err", err.Error()).Error("login:proto:init error")
+//	}
+//}
 
 type ReqLogin struct {
 	Account string `json:"account"`
-	Psw string `json:"psw"`
+	Psw     string `json:"psw"`
 }
 
+type RespLogin struct {
+	Code int    `json:"code"`
+}
--- login/server.go
+++ login/server.go
@@ -1,10 +1,10 @@
 package login
+import "encoding/json"
 
 type LoginServer struct {
 }
 
 func (s *LoginServer)Handle(req string) (resp string, err error) {
-
-
+	err := json.Unmarshal(req, )
 	return "loginServer recive: " + req, nil
 }
