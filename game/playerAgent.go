e61183e6f2cdff36898eed2978e32f8e05309568
mid <86190827@qq.com>
2016-03-11 18:54:22 +0800

    ...

--- game/playerAgent.go
+++ game/playerAgent.go
@@ -2,15 +2,18 @@ package game
 import (
 	"chess/game/cow"
 	"encoding/json"
-"chess/com"
-"github.com/lkj01010/log"
+	"chess/com"
+	"github.com/lkj01010/log"
+	"chess/dao"
 )
 
 type playerAgent struct {
-	connId int
-	c chan string
+	connId   int
+	id       string
+	c        chan string
 	sendFunc func(int, string)
-	game *cow.Game
+
+	toGame chan string		// 发往game的channel
 }
 
 func NewPlayerAgent(connId int, sf func(string)) *playerAgent {
@@ -18,24 +21,25 @@ func NewPlayerAgent(connId int, sf func(string)) *playerAgent {
 		connId: connId,
 		c: make(chan string, 10),
 		sendFunc: sf,
+		toGame: make(chan string, 10),
 	}
 }
 
-func (p *playerAgent)Go(){
+func (p *playerAgent)Go() {
 	for {
 		select {
-		case rcv := <- p.c:
+		case rcv := <-p.c:
 			p.handle(rcv)
 		}
 	}
 }
 
-func (p *playerAgent)Recive(msg string){
+func (p *playerAgent)Recive(msg string) {
 	p.c <- msg
 }
 
 func (p *playerAgent)Send(msg string) {
-	p.sendFunc(msg)
+	p.sendFunc(p.connId, msg)
 }
 
 func (p *playerAgent)handle(msg string) (err error) {
@@ -46,36 +50,53 @@ func (p *playerAgent)handle(msg string) (err error) {
 
 	switch msg.Cmd {
 	case Cmd_Game_EnterReq:
-		_, e := modelInst.playerGames[p.connId]
-		if e == nil {
-			// 已经在游戏中，报错
-			err = com.ErrAlreadyInGame
-			log.Warning("game:model:handle:player enter req, err=", err.Error())
+		err = p.handleEnterReq(msg.Content)
+	default:
+		p.toGame <-msg.Content
+	}
+	return
+}
+
+func (p *playerAgent)handleEnterReq(content string) (err error) {
+	_, e := modelInst.playerGames[p.connId]
+	if e == nil {
+		// 已经在游戏中，报错
+		err = com.ErrAlreadyInGame
+		log.Warning("game:model:handle:player enter req, err=", err.Error())
+		return
+	} else {
+		var content EnterGameReq
+		if err = json.Unmarshal([]byte(content), &content); err != nil {
 			return
-		} else {
-			var content EnterGame
-			if err = json.Unmarshal([]byte(msg.Content), &content); err != nil {
-				return
-			}
-
-			//判断钱是否够
-			modelInst.dao.
-
-			games := modelInst.freeGames[RoomType(content.RoomType)]
-			// 塞进第一个房间
-			var game *cow.Game
-			if len(games) == 0 {
-				// create game
-				game = cow.NewGame(RoomEnterCoin[RoomType(content.RoomType)])
-				game.Go()
-
-				game.PlayerEnter(content.Id)
-			} else {
-				// put to first game
-				game = games[0]
-			}
 		}
-	case cow.Cmd_Cow_Ready:
+		p.id = content.Id
+
+		//判断钱是否够
+		var isCoinEnough bool
+		var reply dao.User_PlayerCoinReply
+		if err = modelInst.dao.Call("User.Coin", dao.User_PlayerCoinArgs{p.id}, &reply); err != nil {
+			return
+		}
+		if reply.Code == com.E_Success {
+			isCoinEnough = (reply.Coin >= RoomEnterCoin[content.RoomType])
+		}else {
+			err = com.ErrRedisValueNotFound
+			return
+		}
+
+		// 够入场费
+		if isCoinEnough {
+			// 塞进房间
+
+			game := modelInst.GetFreeGameByType(content.RoomType)
+			game.PlayerEnter(content.Id, p.toGame, p.Send)
+		}else {
+			// 不够入场费
+			// 返回response
+			resp := com.MakeMsgString(Cmd_Game_EnterResp, com.E_CoinNotEnough, nil)
+			p.Send(resp)
+			return
+		}
 	}
 	return
 }
